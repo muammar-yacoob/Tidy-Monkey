52d9f8a57a479b71aecbb8051a77d0618d2211be
Spark__ <muammar.yacoob@gmail.com>
2021-02-03 19:53:15 +0100

    fix rotation added back

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
 from . tdmk_pt import *
 from . tdmk_op import *
 
-classes = (TITLE_PT_panel, ORGANIZE_PT_panel, CLEANUP_PT_panel, EXPORT_PT_panel, ORG_SELECTED_OT_operator, ORG_ALIGNTOVIEW_OT_operator, ORG_CENTER_OT_operator, BUTTS_OT_operator, ALIGN_OT_operator, REN_BONES_OT_operator, SELECT_SAME_OT_operator, CLEAR_MATS_OT_operator, CLEAN_TEX_OT_operator, GEN_ACTS_OT_operator, EXPORT_OT_operator, SHARE_OT_operator,REN_VERT_OT_operator)
+classes = (TITLE_PT_panel, ORGANIZE_PT_panel, CLEANUP_PT_panel, EXPORT_PT_panel, ORG_SELECTED_OT_operator, ORG_ALIGNTOVIEW_OT_operator, ORG_CENTER_OT_operator, BUTTS_OT_operator, ALIGN_OT_operator, REN_BONES_OT_operator, SELECT_SAME_OT_operator, CLEAR_MATS_OT_operator, CLEAN_TEX_OT_operator, GEN_ACTS_OT_operator, EXPORT_OT_operator, SHARE_OT_operator,REN_VERT_OT_operator,ORG_FIXROTATION_OT_operator)
 
 register, unregister = bpy.utils.register_classes_factory(classes)
 
