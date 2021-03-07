d4ec79b52816f2a62bbd1a7b541bfcfd7afb1958
Muammar81 <Muammar.Yacoob@gmail.com>
2021-03-07 15:47:19 +0100

    Update tdmk_op.py
    Export animated bones

--- tdmk_op.py
+++ tdmk_op.py
@@ -561,7 +561,16 @@ class EXPORT_OT_operator(bpy.types.Operator):
                 hasShapeKey = False
                 
             hasShapeKey = bool(obj.type == 'ARMATURE') or bool(hasShapeKey)
-            
+
+            #push all acts down
+            #bpy.context.area.type = "NLA_EDITOR"
+            for obj in sel_objs:            
+                if obj.animation_data is not None:
+                    action = obj.animation_data.action
+                    if action is not None:
+                        track = obj.animation_data.nla_tracks.new()
+                        track.strips.new(action.name, action.frame_range[0], action)
+                        obj.animation_data.action = None            
 
             for child in obj.children:
                 child.select_set(state=True)
