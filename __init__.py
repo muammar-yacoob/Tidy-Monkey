3a74539fab6c6d021862a1be1449e9f864f0ffb0
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2025-05-04 20:24:21 +0100

    restructured

--- __init__.py
+++ __init__.py
@@ -1,4 +1,26 @@
-from .tidy_monkey import register, unregister
+import bpy
+from . import src
 
-__register__ = register
-__unregister__ = unregister 
+class TIDYMONKEY_PT_MinimalPanel(bpy.types.Panel):
+    bl_label = "Tidy Monkey"
+    bl_idname = "TIDYMONKEY_PT_minimal_panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Tidy Monkey"
+    
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text="Tidy Monkey is ready!")
+
+def register():
+    print("Registering Tidy Monkey")
+    bpy.utils.register_class(TIDYMONKEY_PT_MinimalPanel)
+    src.register()
+    
+def unregister():
+    print("Unregistering Tidy Monkey")
+    src.unregister()
+    bpy.utils.unregister_class(TIDYMONKEY_PT_MinimalPanel)
+
+if __name__ == "__main__":
+    register() 
