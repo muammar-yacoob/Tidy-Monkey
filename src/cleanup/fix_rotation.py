c9d13ef1fa652a2e2ccf4dad31a0dc34d80d4e9e
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2025-05-06 20:28:08 +0100

    prefect rotation applied

--- src/cleanup/fix_rotation.py
+++ src/cleanup/fix_rotation.py
@@ -28,14 +28,16 @@ class FIXROTATION_OT_operator(bpy.types.Operator):
             return {'CANCELLED'}
         
         face = bm.faces.active if bm.faces.active and bm.faces.active.select else selected_faces[0]
-        
         face_normal = face.normal.copy()
-        face_normal_world = obj.matrix_world.to_3x3() @ face_normal
-        face_normal_world.normalize()
+        
+        bmesh.update_edit_mesh(obj.data)
         
         bpy.ops.object.mode_set(mode='OBJECT')
         
         orig_loc = obj.location.copy()
+        
+        face_normal_world = obj.matrix_world.to_3x3() @ face_normal
+        face_normal_world.normalize()
         
         target_axis = Vector((0, 0, -1))
         rotation = face_normal_world.rotation_difference(target_axis)
